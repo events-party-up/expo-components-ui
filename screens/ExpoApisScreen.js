ee9e79168af8a0db5a20e760afa7474ba2d37508
Brent Vatne <brentvatne@gmail.com>
2017-11-14 20:56:47 +0000

    Only show adjustsFontSizeToFit example on iOS
    fbshipit-source-id: 4ca584e

--- screens/ExpoApisScreen.js
+++ screens/ExpoApisScreen.js
@@ -238,9 +238,11 @@ export default class ExpoApisScreen extends React.Component {
           <Text style={{ fontFamily: 'space-mono', fontSize: 16 }}>
             Font icons sets and other custom fonts can be loaded from the web
           </Text>
-          <Text adjustsFontSizeToFit style={{ fontFamily: 'space-mono', fontSize: 400 }}>
-            Custom fonts can be used with `adjustsFontSizeToFit`
-          </Text>
+          {Platform.OS === 'ios' ? (
+            <Text adjustsFontSizeToFit style={{ fontFamily: 'space-mono', fontSize: 400 }}>
+              Custom fonts can be used with `adjustsFontSizeToFit` on iOS
+            </Text>
+          ) : null}
         </View>
       </View>
     );
