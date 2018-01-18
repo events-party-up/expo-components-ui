1166f3d91c94d90259af5bad01ac1b31d05f6a99
Eric Samelson <samelson.e@gmail.com>
2018-01-18 03:20:34 +0000

    add missing imports to DocumentPickerScreen
    fbshipit-source-id: 4564925

--- screens/DocumentPickerScreen.js
+++ screens/DocumentPickerScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, ScrollView, View } from 'react-native';
+import { Alert, Button, Image, ScrollView, Text, View } from 'react-native';
 import { DocumentPicker } from 'expo';
 
 export default class DocumentPickerScreen extends React.Component {
