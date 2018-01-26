733a712b3ffd8ef02996324dd180059e7170c645
Ben Roth <spongebucket@gmail.com>
2018-01-26 20:21:23 +0000

    re-add AdMobScreen with new capitalization
    fbshipit-source-id: 6a52dc2

---
+++ screens/AdMobScreen.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, View, NativeModules, Platform, TouchableHighlight } from 'react-native';
+import Expo, { AdMobBanner } from 'expo';
+import { Colors } from '../constants';
+
+const AdMobRewarded = NativeModules.RNAdMobRewarded;
+const AdMobInterstitial = Expo.AdMobInterstitial;
+
+export default class AdMobScreen extends React.Component {
+  constructor() {
+    super();
+    AdMobRewarded.setTestDeviceID('EMULATOR');
+    AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+    AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+    AdMobInterstitial.setTestDeviceID('EMULATOR');
+  }
+
+  componentDidMount() {
+    AdMobRewarded.requestAd(error => error && console.log(error));
+    AdMobInterstitial.requestAd(error => error && console.log(error));
+  }
+
+  onPress() {
+    AdMobRewarded.showAd(error => error && console.log(error));
+  }
+
+  onInterstitialPress() {
+    AdMobInterstitial.showAd(error => error && console.log(error));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <TouchableHighlight style={styles.button}>
+            <Text onPress={this.onPress} style={styles.buttonText}>
+              Show Rewarded Interstitial Ad
+            </Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.button}>
+            <Text onPress={this.onInterstitialPress} style={styles.buttonText}>
+              Show Interstitial Ad
+            </Text>
+          </TouchableHighlight>
+          <AdMobBanner
+            bannerSize="banner"
+            adUnitID="ca-app-pub-3940256099942544/6300978111"
+            testDeviceID="EMULATOR"
+            didFailToReceiveAdWithError={this.bannerError}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'ios' ? 30 : 30,
+    flex: 1,
+    alignItems: 'center',
+  },
+  button: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 3,
+    backgroundColor: Colors.tintColor,
+    marginRight: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
