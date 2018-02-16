4d90e8cb1ac3019f3489265576984f413f4e2906
Ben Roth <spongebucket@gmail.com>
2018-02-16 05:02:42 +0000

    Standardize localization styles
    fbshipit-source-id: b9e0090

--- screens/LocalizationScreen.js
+++ screens/LocalizationScreen.js
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View, Picker, ScrollView, Platform } from 'react-nati
 import Expo from 'expo';
 import chunk from 'lodash/chunk';
 
+import HeadingText from '../components/HeadingText';
 import MonoText from '../components/MonoText';
-import Button from '../components/Button';
+import ListButton from '../components/ListButton';
 
 const { Localization } = Expo;
 
@@ -72,26 +73,27 @@ export default class LocalizationScreen extends React.Component {
     this.localeStore.setLocale(locale, () => this.setState(() => ({ locale })));
 
   render() {
+    const preferredLocales = (this.state.preferredLocales && this.state.preferredLocales.length)
+          ? (<MonoText>{JSON.stringify(this.state.preferredLocales, null, 2)}</MonoText>)
+          : null;
+    const currencyCodes = (this.state.isoCurrencyCodes && this.state.isoCurrencyCodes.length)
+          ? (<MonoText>{this.prettyFormatCurrency()}</MonoText>)
+          : null;
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Button title={'Show preferred Locales'} onPress={this.queryPreferredLocales}>
-            <Text style={styles.isoCurrencyCodes}>Query Platform values</Text>
-          </Button>
-          <View style={styles.centered}>
-            <Text style={styles.plainBanner}>Current Locale</Text>
-            <MonoText>{JSON.stringify(this.state.currentLocale, null, 2)}</MonoText>
-          </View>
-          <View style={styles.centered}>
-            <Text style={styles.plainBanner}>Locales in Preference Order</Text>
-            <MonoText>{JSON.stringify(this.state.preferredLocales, null, 2)}</MonoText>
-          </View>
-          <Button title={'Show first 100 currency codes'} onPress={this.queryCurrencyCodes}>
-            <Text style={styles.isoCurrencyCodes}>First 100 ISO currency codes</Text>
-          </Button>
-          <MonoText textStyle={styles.centeredText} style={styles.centered}>
-            {this.prettyFormatCurrency()}
-          </MonoText>
+          <HeadingText>Current Locale</HeadingText>
+          <MonoText>{JSON.stringify(this.state.currentLocale, null, 2)}</MonoText>
+          
+          <HeadingText>Locales in Preference Order</HeadingText>
+          <ListButton title={'Show preferred Locales'} onPress={this.queryPreferredLocales} />
+          {preferredLocales}
+
+          <HeadingText>Currency Codes</HeadingText>
+          <ListButton title={'Show first 100 currency codes'} onPress={this.queryCurrencyCodes} />
+          {currencyCodes}
+
+          <HeadingText>Localization Table</HeadingText>
           <Picker
             style={styles.picker}
             selectedValue={this.state.locale}
@@ -102,8 +104,9 @@ export default class LocalizationScreen extends React.Component {
               value={(Platform.OS === 'ios' && 'ru_US') || (Platform.OS === 'android' && 'ru_RU')}
             />
           </Picker>
-          <Text style={styles.plainBanner}>Localization table</Text>
+          
           <MonoText>{JSON.stringify(localization, null, 2)}</MonoText>
+          
           <View style={styles.languageBox}>
             <View style={styles.row}>
               <Text>Exists in Both: </Text>
@@ -120,7 +123,6 @@ export default class LocalizationScreen extends React.Component {
   }
 }
 const styles = StyleSheet.create({
-  centered: { alignItems: 'center', justifyContent: 'center' },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -129,18 +131,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
   },
-  isoCurrencyCodes: {
-    padding: 10,
-    fontSize: 24,
-    backgroundColor: 'aliceblue',
+  picker: {
     borderWidth: 1,
+    padding: 0,
+    margin: 0,
   },
-  picker: { backgroundColor: 'aliceblue', width: '50%', borderWidth: 1 },
   container: {
-    paddingVertical: '10%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
-  plainBanner: { fontSize: 18 },
-  centeredText: { textAlign: 'center' },
 });
